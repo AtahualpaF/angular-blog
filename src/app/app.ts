bd7c9654e578584aa841cda3a883a67b863d4e2e
Atahualpa Fagundes de Souza Assis <a.fagundesassis2@gmail.com>
2025-10-30 09:07:47 -0300

    componentes e menu title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitle } from "./components/menu-title/menu-title";
+import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitle, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
